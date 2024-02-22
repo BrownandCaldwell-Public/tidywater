da63237a4643aa6b081475d00e44b35a4634e1bd
Riley E. Mulhern <rmulhern@brwncald.com>
2024-02-22 13:13:29 -0700

    dose_target and dose_chemical documentation
    Clean up documentation for dose_target and dose_chemical, including simple examples.

--- R/acid_base_equilibirum.R
+++ R/acid_base_equilibirum.R
@@ -1,9 +1,5 @@
 # Acid/Base Equilibrium Functions
-# These functions determine pH and alkalinity after blending and chemical addition
-
-# Author: R. Mulhern, based on spreadsheet calculations by C. Corwin
-# Reviewers: Sierra Johnson 1/19/24
-
+# These functions determine pH and alkalinity after blending and chemical addition.
 
 #### Function to calculate the pH from a given water quality vector. Not exported in namespace.
 
@@ -34,33 +30,52 @@ solve_ph <- function(water, so4_dose = 0, po4_dose = 0, na_dose = 0, ca_dose = 0
   return(round(phfinal, 2))
 }
 
-#' Chemical Dose Function
+#' Add chemicals to water
 #'
-#' This function takes chemical doses and a water data frame defined by \code{\link{define_water}} and outputs a new water data frame with updated ion balance and pH.
-#' Units of all chemical additions in mg/L as chemical (not as product).
-#' Returns "water" class object of dosed water.
+#' \code{dose_chemical} calculates the new pH, alkalinity, and ion balance of a water based on different chemical
+#' additions. The function takes an object of class "water" created by \code{\link{define_water}} and user-specified
+#' chemical additions and returns a new object of class "water" with updated water quality.
+#' Units of all chemical additions are in mg/L as chemical (not as product).
 #'
-#' @param water Source water of class "water" created by \code{\link{define_water}}
-#' @param hcl Hydrochloric acid: HCl -> H + Cl
-#' @param h2so4 Sulfuric acid: H2SO4 -> 2H + SO4
-#' @param h3po4 Phosphoric acid: H3PO4 -> 3H + PO4
-#' @param naoh Caustic: NaOH -> Na + OH
-#' @param na2co3 Soda ash: Na2CO3 -> 2Na + CO3
-#' @param nahco3 Sodium bicarbonate: NaHCO3 -> Na + H + CO3
-#' @param caoh2 Lime: Ca(OH)2 -> Ca + 2OH
-#' @param mgoh2  Magneisum hydroxide: Mg(OH)2 -> Mg + 2OH
-#' @param cl2 Chlorine gas: Cl2(g) + H2O -> HOCl + H + Cl
-#' @param naocl Sodium hypochlorite: NaOCl -> Na + OCl
-#' @param caocl2 Calcium hypochlorite: Ca(OCl)2 -> Ca + 2OCl
-#' @param co2 Carbon Dioxide CO2 (gas) + H2O -> H2CO3*
-#' @param alum Hydrated aluminum sulfate Al2(SO4)3*14H2O + 6HCO3 -> 2Al(OH)3(am) +3SO4 + 14H2O + 6CO2
-#' @param fecl3 Ferric Chloride FeCl3 + 3HCO3 -> Fe(OH)3(am) + 3Cl + 3CO2
-#' @param fe2so43 Ferric sulfate Fe2(SO4)3 + 6HCO3 -> 2Fe(OH)3(am) +3SO4 + 6CO2
+#' \code{dose_chemical} works by evaluating all the user-specified chemical additions and solving for what the new pH
+#' must be using \code{uniroot} to satisfy the principle of electroneutrality in pure water while correcting for the existing alkalinity
+#' of the water that the chemical is added to. Multiple chemicals can be added simultaneously of each addition can be
+#' modeled independently through sequential doses.
 #'
-#' @seealso \code{\link{define_water}}
+#' @param water Source water object of class "water" created by \code{\link{define_water}}
+#' @param hcl Amount of hydrochloric acid added in mg/L: HCl -> H + Cl
+#' @param h2so4 Amount of sulfuric acid added in mg/L: H2SO4 -> 2H + SO4
+#' @param h3po4 Amount of phosphoric acid added in mg/L: H3PO4 -> 3H + PO4
+#' @param naoh Amount of caustic added in mg/L: NaOH -> Na + OH
+#' @param na2co3 Amount of soda ash added in mg/L: Na2CO3 -> 2Na + CO3
+#' @param nahco3 Amount of sodium bicarbonate added in mg/L: NaHCO3 -> Na + H + CO3
+#' @param caoh2 Amount of lime added in mg/L: Ca(OH)2 -> Ca + 2OH
+#' @param mgoh2  Amount of magneisum hydroxide added in mg/L: Mg(OH)2 -> Mg + 2OH
+#' @param cl2 Amount of chlorine gas added in mg/L: Cl2(g) + H2O -> HOCl + H + Cl
+#' @param naocl Amount of sodium hypochlorite added in mg/L: NaOCl -> Na + OCl
+#' @param caocl2 Amount of calcium hypochlorite added in mg/L: Ca(OCl)2 -> Ca + 2OCl
+#' @param co2 Amount of carbon dioxide added in mg/L: CO2 (gas) + H2O -> H2CO3*
+#' @param alum Amount of hydrated aluminum sulfate added in mg/L: Al2(SO4)3*14H2O + 6HCO3 -> 2Al(OH)3(am) +3SO4 + 14H2O + 6CO2
+#' @param fecl3 Amount of ferric Chloride added in mg/L: FeCl3 + 3HCO3 -> Fe(OH)3(am) + 3Cl + 3CO2
+#' @param fe2so43 Amount of ferric sulfate added in mg/L: Fe2(SO4)3 + 6HCO3 -> 2Fe(OH)3(am) +3SO4 + 6CO2
+#'
+#' @seealso \code{\link{define_water}}, \code{\link{convert_units}}
 #'
 #' @examples
-#' # Put example code here
+#' water <- define_water(ph = 7, temp = 25, alk = 10)
+#' # Dose 1 mg/L of hydrochloric acid
+#' dosed_water <- dose_chemical(water, hcl = 1)
+#' dosed_water@ph
+#'
+#' #Dose 1 mg/L of hydrochloric acid and 5 mg/L of alum simultaneously
+#' dosed_water <- dose_chemical(water, hcl = 1, alum = 5)
+#' dosed_water@ph
+#'
+#'#Dose 1 mg/L of hydrochloric acid and 5 mg/L of alum sequentially
+#' dosed_water1 <- dose_chemical(water, hcl = 1)
+#' dosed_water1@ph
+#' dosed_water2 <- dose_chemical(dosed_water1, alum = 5)
+#' dosed_water2@ph
 #'
 #' @export
 #'
@@ -71,7 +86,7 @@ dose_chemical <- function(water, hcl = 0, h2so4 = 0, h3po4 = 0, naoh = 0, na2co3
   if (missing(water)) {
     stop("No source water defined. Create a water using the 'define_water' function.")}
   if(class(water) != "water") {
-    stop("Input water must be of class 'water'. Create a water using define_water.")
+    stop("Input water must be of class 'water'. Create a water using 'define_water'.")
   }
   #### CONVERT INDIVIDUAL CHEMICAL ADDITIONS TO MOLAR ####
 
@@ -183,19 +198,26 @@ dose_chemical <- function(water, hcl = 0, h2so4 = 0, h3po4 = 0, naoh = 0, na2co3
 
 
 
-#' Target Chemical Dose Function
+#' Calculate a desired chemical dose for a target pH
 #'
-#' This function calculates the required amount of a chemical to dose based on a target pH and existing water quality.
-#' Returns numeric value for dose in mg/L. Uses optimize on the dose_chemical function.
+#' \code{dose_target} calculates the required amount of a chemical to dose based on a target pH and existing water quality.
+#' The function takes an object of class "water" created by \code{\link{define_water}}, and user-specified chemical and target
+#' and returns a numeric value for the required dose in mg/L.
+#'
+#' \code{dose_target} uses \code{uniroot} on \code{\link{dose_chemical}} to match the required dose for the requested pH target.
 #'
 #' @param water Source water of class "water" created by \code{\link{define_water}}
 #' @param target_ph The final pH to be achieved after the specified chemical is added.
-#' @param chemical The chemical to be added. Current supported chemicals include: caustic soda (NaOH), lime (Ca(OH2)), magnesium hydroxide (Mg(OH)2), and CO2.
+#' @param chemical The chemical to be added. Current supported chemicals include: caustic soda ("naoh"), lime ("caoh2"),
+#' magnesium hydroxide ("mgoh2"), and carbon dioxide ("co2").
 #'
-#' @seealso \code{\link{define_water}}
+#' @seealso \code{\link{define_water}}, \code{\link{dose_chemical}}
 #'
 #' @examples
-#' # Put example code here
+#' water <- define_water(ph = 7, temp = 25, alk = 10)
+#'
+#' # Calculate required dose of lime to reach pH 8
+#' dose_target(water, target_ph = 8, chemical = "caoh2")
 #'
 #' @export
 #'
