7770decd5a9c651962ad9f00e988e763f57c5aab
Libby McKenna <libbydmckenna@gmail.com>
2024-03-26 18:04:23 -0600

    add testthat and allow for multiple species selection

--- R/dbp_models.R
+++ R/dbp_models.R
@@ -1,14 +1,13 @@
 # DBP Modeling functions
-# These functions help predict total trihalomethane (TTHM) and haloacetic acid (HAA) formation
-
+# These functions predict total trihalomethane (TTHM) and haloacetic acid (HAA) formation
 
 #' Calculate DBP formation using dosed chlorine, ambient bromide, and reaction time.
 #'
-#' \code{chemdose_dbp} calculates disinfection byproduct (DBP) formation based on
-#' chlorine addition, bromide, TOC, UV254, bromide, temperature, pH, and reaction time. This function will 
-#' calculate haloacetic acids (HAA) as HAA5 or HAA6, as well as total trihalomethanes (TTHM).
+#' \code{chemdose_dbp} calculates disinfection byproduct (DBP) formation based on the WTP model. Parameters required
+#' include chlorine addition, bromide, TOC, UV254, temperature, pH, and reaction time. This function will 
+#' calculate haloacetic acids (HAA) as HAA5, HAA6, or HAA9, as well as total trihalomethanes (TTHM).
 #' The function takes an object of class "water" created by \code{\link{define_water}} and user-specified
-#' chlorine addition and returns a dataframe of predicted DBP formation.
+#' chlorine addition, bromide, and reaction time and returns a dataframe of predicted DBP formation.
 #' 
 #' TTHMs, untreated: Amy et al. (1998), WTP Model v. 2.0, equation 5-131
 #' HAAs, untreated: Amy et al. (1998), WTP Model v. 2.0, equation 5-134 
@@ -18,15 +17,20 @@
 #'
 #' @param water Source water object of class "water" created by \code{\link{define_water}}
 #' @param cl2 applied chlorine dose (mg/L as Cl2). Dose should be between 1.51 and 33.55 mg/L
-#' @param br bromide concentration (ug/L). Concentration should be between 7 and 600 ug/L
+#' @param br bromide (br-) concentration (ug/L). Concentration should be between 7 and 600 ug/L
 #' @param time reaction time (hours). Reaction time should be between 2 and 168 hours
 #' @param water_type type of treatment applied to the water. Default type is "treated", but
 #' user may also specify "untreated". Untreated water is generally raw water. "treated water applies to 
 #' water that has been coagulated or softened.
 #' @param species the dbp species or group of species that should be modeled. Default species is "tthm", but
 #' user may also specify haa5, haa6, haa9, or any of the species within these groups. Note: haa9 and its subspecies
-#' are only available when water_type = "treated".
+#' are only available when water_type = "treated". For a list of all species names, see dbpcoeffs dataframe. Multiple
+#' species may be selected using c().
 #' @examples
+#' example_dbp <- suppressWarnings(define_water(7.5, 20, 66, toc = 4, uv254 = .2)) %>%
+#' chemdose_dbp(cl2 = 2, br = 30, time = 8)
+#'example_dbp <- suppressWarnings(define_water(8, 25, 66, toc = 4, uv254 = .2)) %>%
+#'chemdose_dbp(cl2 = 2, br = 30, time = 8, water_type = "untreated", species = c("tthm", "chcl3", "chcl2br", "chbr2cl", "chbr3"))
 #'
 #' @export
 #'
@@ -48,7 +52,7 @@ chemdose_dbp <- function(water, cl2, br, time, water_type = "treated", species =
     stop("Missing value for cl2, br, or time. Please check the function inputs required to calculate DBP formation.")
   }
   
-  if (toc < 1.2 | toc > 10.6) {
+  if (water_type == "untreated" & (toc < 1.2 | toc > 10.6)) {
     warning("TOC is outside the model bounds of 1.2 <= toc <= 10.6 mg/L as set in the WTP model.")
   }
   if (water_type == "treated" & (doc < 1.00 | doc > 7.77)) {
@@ -58,7 +62,6 @@ chemdose_dbp <- function(water, cl2, br, time, water_type = "treated", species =
   if (water_type == "untreated" & (uv254 < 0.01 | uv254 > 0.318)) {
     warning("UV254 is outside the untreated water model bounds of 0.01 <= uv254 <= 0.318 cm-1 as set in the WTP model.")
   }
-  
   if (water_type == "treated" & (uv254 < 0.016 | uv254 > 0.215)) {
     warning("UV254 is outside the treated water model bounds of 0.016 <= uv254 <= 0.215 cm-1 as set in the WTP model.")
   }
@@ -87,7 +90,7 @@ chemdose_dbp <- function(water, cl2, br, time, water_type = "treated", species =
   if (water_type == "untreated" & (ph < 6.5 | ph > 8.5)) {
     warning("pH is outside the untreated water model bounds of 6.5 <= ph <= 8.5 as set in the WTP model.")
   }
-  if (water_type == "untreated" & ph != 7.5) {
+  if (water_type == "treated" & ph != 7.5) {
     warning("pH is not set to 7.5 as set in the WTP model for treated water modeling.")
   }
 
@@ -99,7 +102,7 @@ chemdose_dbp <- function(water, cl2, br, time, water_type = "treated", species =
 
     predicted_dbp <- dbpcoeffs %>%
       filter(water_type == "untreated") %>%
-      filter(ID == species) %>%
+      filter(ID %in% species) %>%
       mutate(modeled_dbp_ug.L = A * toc^a * cl2^b * br^c * temp^d * ph^e * time^f)%>%
       select(-c(A:f))
   } 
@@ -108,7 +111,7 @@ chemdose_dbp <- function(water, cl2, br, time, water_type = "treated", species =
     
     predicted_dbp <- dbpcoeffs %>%
       filter(water_type == "treated") %>%
-      filter(ID == species) %>%
+      filter(ID %in% species) %>%
       mutate(modeled_dbp_ug.L = A * (doc*uv254)^a * cl2^b * br^c * d^(ph-7.5) * e^(temp-20) * time^f) %>%
       select(-c(A:f))
   }
@@ -116,11 +119,3 @@ chemdose_dbp <- function(water, cl2, br, time, water_type = "treated", species =
 
   return(predicted_dbp)
 }
-# 
-water <- define_water(8, 25, 66, toc = 4, uv254 = .2)
-cl2 = 2
-br=8
-time=5
-species = "tthm"
-
-  test <- chemdose_dbp(water, cl2=2, br = 30, time = 168)
