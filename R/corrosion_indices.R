bb0969258054248435a20c76e72a8653733117d0
Libby McKenna <libbydmckenna@gmail.com>
2024-04-09 17:13:39 -0600

    Trying trussell's ccpp method

--- R/corrosion_indices.R
+++ R/corrosion_indices.R
@@ -103,7 +103,8 @@ calculate_corrosion <- function(water, index = c("aggressive", "ryznar", "langel
 
   ##############################*
   # USE CHRIS' SPREADSHEET TO CALC LSI ----
-
+  
+# these activities are a bit different than what Chris has calc'd
   active_1 = calculate_activity(1, water@is, tempa) 
   active_2 = calculate_activity(2, water@is, tempa)
   alk_mol = convert_units(water@alk, "caco3", startunit = "mg/L CaCO3")
@@ -121,21 +122,22 @@ calculate_corrosion <- function(water, index = c("aggressive", "ryznar", "langel
   
   # Langelier, W. (1936) "The Analytical Control of Anti-Corrosion Water Treatment,"
       # J. AWWA, 28, 11, 1500–1522.
-  a_h = water@kw/water@oh
-  K.2 = a_h*water@co3 /water@hco3  
+  a_h = water@kw/water@oh #MWH eq 22-25
+  K.2 = a_h*water@co3 /water@hco3   #MWH eq # 22-24
     
-  log_k2_libby = log10(K.2)
+  log_k2 = -log10(K.2)
   
   #mixed solubility constant for CaCO3
   log_kso = 171.9065 + 0.077993*tempa - 2839.319/tempa - 71.595*log10(tempa) #From Plummer and Busenberg (1982), MWH table 22-9
+  # log_kso = -log10(water@ca * water@co3) #MWH eq 22-23
   
-  ph_s_mwh = -log_k2_libby - log_kso - log10(water@ca) - log10(water@alk_eq) # from MWH eq 22-30
+  ph_s_mwh = log_k2 - log_kso - log10(water@ca) - log10(alk_mol) # from MWH eq 22-30
   water@ph - ph_s_mwh
 
   water@ph - ph_s_awwa  #-1.95
   water@ph - ph_s_chris #-1.92
-  water@ph - ph_s_mwh   #-1.38
-  
+  water@ph - ph_s_mwh   #-1.68
+  ph_s = ph_s_chris
 # NOTE!!!
   # Original paper has alk in equivalence. Chris and other use mols
   
@@ -164,28 +166,83 @@ calculate_corrosion <- function(water, index = c("aggressive", "ryznar", "langel
   # check against Chris's spreadsheet
   # https://legacy.azdeq.gov/environ/water/dw/download/vol_II_app_abc.pdf
   # https://awwa.onlinelibrary.wiley.com/doi/pdfdirect/10.1002/j.1551-8833.1998.tb08471.x
-  if ("ccpp" %in% index) {
-     Ca_est = (ca_hard-water@langelier*20)/100.869/1000 # cell A821
-     IS = water@is - 4*(water@ca - Ca_est) # cell C821
-     activity_1 =  calculate_activity(1, IS, tempa) # cell D821
-     activity_2 =  calculate_activity(2, IS, tempa) # cell E821
-     activity_3 =  calculate_activity(3, IS, tempa) # cell F821
-     tot_co3 = water@tot_co3 - (water@ca - Ca_est) # cell G821, mol/L
-     Cb_Ca = (water@hco3 + 2*water@co3 + water@oh - water@h) - 2 * (water@ca - Ca_est) #cell H821
-    h_conc = solve_h?  #cell I821 - solve_H?
-      ph = -log10(h_conc) #cell J821
-    k = correct_k(water)
-    a_1 = calculate_alpha1_carbonate(h_conc, k) #cell K821
-    a_2 = calculate_alpha2_carbonate(h_conc, k) #cell L821
-    Ca_sol = (10^(-log_kso))/(activity_2^2)/a_2/tot_co3*(1+(10^-water@kw)/activity_1^2 * (10^-T48)*activity_2/h_conc +
-                                                       (10^-T49) *activity_2*a_1*tot_co3 +
-                                                       (10^-T50)*activity_2^2*a_2*tot_co3 +
-                                                       (10^-T51) * activity_2^2*0)  #cell M821
-    C1_C2 = (Ca_est - Ca_sol) * 1000000 #cell N821
-    LHS_RHS = Ca_sol/Ca_est#cell O821
-    Ca = convert_units(Ca_sol, "ca", "M", "mg/L CaCO3") #cell P821
-    CCPP = ca_hard - Ca #cell Q821
-  }
+  # if ("ccpp" %in% index) {
+  #    Ca_est = (ca_hard-water@langelier*20)/100.869/1000 # cell A821
+  #    IS = water@is - 4*(water@ca - Ca_est) # cell C821
+  #    
+  #    # these activities don't quite match what Chris calculates
+  #    activity_1 =  calculate_activity(1, IS, tempa) # cell D821
+  #    activity_2 =  calculate_activity(2, IS, tempa) # cell E821
+  #    activity_3 =  calculate_activity(3, IS, tempa) # cell F821
+  #    tot_co3 = water@tot_co3 - (water@ca - Ca_est) # cell G821, mol/L
+  #    Cb_Ca = (water@hco3 + 2*water@co3 + water@oh - water@h) - 2 * (water@ca - Ca_est) #cell H821
+  #    
+  #    # I think I should use solve_pH here, but this fn uses calc_alphas. Chris's spreadsheet uses activities. how to incorporate?
+  #    ph = 9.9 #solve_ph(water) #cell J821
+  #    h_conc = 10^-ph #cell I821 - solve_H?
+  #    
+  #   a_1 = calculate_alpha1_carbonate(h_conc, k) #cell K821
+  #   a_2 = calculate_alpha2_carbonate(h_conc, k) #cell L821
+  #   
+  #   
+  #   Ca_sol = (10^(-log_kso))/(activity_2^2)/a_2/tot_co3*(1+(10^-water@kw)/activity_1^2 * (10^-T48)*activity_2/h_conc +
+  #                                                      (10^-T49) *activity_2*a_1*tot_co3 +
+  #                                                      (10^-T50)*activity_2^2*a_2*tot_co3 +
+  #                                                      (10^-T51) * activity_2^2*0)  #cell M821
+  #   C1_C2 = (Ca_est - Ca_sol) * 1000000 #cell N821
+  #   LHS_RHS = Ca_sol/Ca_est#cell O821
+  #   Ca = convert_units(Ca_sol, "ca", "M", "mg/L CaCO3") #cell P821
+  #   CCPP = ca_hard - Ca #cell Q821
+  # }
   
+  
+  
+  # using Trussell
+  water <- define_water(ph = 8, temp = 25, alk = 220, ca_hard = 200)
+  if ("ccpp" %in% index) {
+    
+    #before precipitation
+    water1 <- define_water(ph = 8, temp = 25, alk = 220, ca_hard = 200) #initial pH
+    ca = convert_units(water1@ca, "ca", "M", "mM")
+    alk_meq = water1@alk_eq *1000 #meq/L
+    #From Trussell eq S4. Charge coefficient for carbonate species in eq/mol.
+    sig_co2 = (water1@hco3 + 2 * water1@co3)/water1@tot_co3 #meq/mol
+    #electroneutrality shift, equation S6
+    delta = (water1@oh - water1@h) *1000 #meq/L
+    tot_co3 = water1@tot_co3 * 1000    # mmol/L
+    Kso = ca * water1@co3*1000
+    k = correct_k(water1)
+    a_2 = calculate_alpha2_carbonate(water1@h, k)
+    x = (tot_co3*sig_co2 -delta - alk_meq) / (sig_co2 - 2)
+    pH_i = (ca - x) * (tot_co3 -x) *a_2  - Kso
+    ph = 4 #initial guess
+    
+    # while(ph > 7) {
+      
+    # after precipitation
+    water_loop <- define_water(ph = ph, temp = 25, alk = 220, ca_hard = 200)
+    ca_loop = convert_units(water_loop@ca, "ca", "M", "mM")
+    alk_meq_loop = water_loop@alk_eq *1000 #meq/L
+    sig_co2_loop  = (water_loop@hco3 + 2 * water_loop@co3)/water_loop@tot_co3 #meq/mol
+    delta_loop  = (water_loop@oh - water_loop@h) *1000 #meq/L
+    tot_co3_loop  = water_loop@tot_co3 * 1000    # mmol/L
+    Kso_loop  = ca_loop* water_loop@co3*1000
+    k_loop  = correct_k(water_loop)
+    a_2_loop  = calculate_alpha2_carbonate(water_loop@h, k)
+    
+    x_loop = (tot_co3_loop*sig_co2_loop -delta_loop - alk_meq_loop) / (sig_co2_loop - 2)
+    # x= -.01
+
+    pH_f = (ca_loop - x_loop) * (tot_co3_loop -x_loop) *a_2  - Kso_loop
+    
+    err = pH_i - pH_f
+    err
+    ph = ph - 0.1
+    
+      
+    }
+    
+    
+  }
   
 }
