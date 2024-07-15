b1edf0aaef0e7f5b04a3590eb951b3aa7998337e
rmerrifield1 <rmerrifield@brwncald.com>
2024-07-15 09:59:09 -0700

    Add files via upload
    Attached is my code for the tests and the ggplots. My R is being funky and not letting me run it so any input would be great.

---
+++ PAC_tests.R
@@ -0,0 +1,66 @@
+library(testthat)
+library(tidywater)
+#install use this to set up test file
+intall.packages("usethis")
+library(usethis)
+usethis:use_test("Pac_toc")
+test_check("tidywater")
+
+# PAC_TOC ----
+
+test_that("pac_toc returns no modeled DOC value when PAC dose is 0.", {
+  water1 <- suppressWarnings(define_water(doc=2.5, uv254=.05,toc=3.5))
+  water2 <- suppressWarnings(pac_toc(water1,type="wood", time = 8,dose=0))
+  #expected value of doc when no PAC is added - would equal starting value 
+  expect_equal(water2@doc, 2.5)
+})
+
+#test that function handles wrong pac type - example is "invalid" 
+test_that("pac_toc does not run when type isn't specified correctly.", {
+  water1 <- suppressWarnings(define_water(doc=2.5, uv254=.05,toc=3.5))
+  
+  expect_error(pac_toc(water1, dose=15, time=50, type = "invalid"))
+})
+
+test_that("pac_toc warns when inputs are out of model range", {
+  water1 <- suppressWarnings(define_water(doc=2.5, uv254=.05,toc=50))
+  
+  expect_warning(pac_toc(water1, dose=31, time = 50)) # dose is out of bounds
+  expect_warning(pac_toc(water1, dose=15, time = 1441)) # time out of bounds
+ 
+})
+
+
+test_that("pac_toc stops working when inputs are missing", {
+  water1 <- suppressWarnings(define_water(doc=2.5, uv254=.05,toc=3.5))
+  
+  expect_error(pac_toc(water1, dose=15)) # missing time
+  expect_error(pac_toc(water1, time=50)) # missing dose
+  expect_error(pac_toc(dose=15, time=50)) # missing water
+  expect_no_error(suppressWarnings(pac_toc(water1, dose=15,time=50, type="wood"))) # runs without errors with all inputs given correctly
+})
+  
+test_that("Input water is s4 class", { 
+water1 <-supressWarnings(define_water(doc=3.5,uv254=.1))
+dosed_water <- pac_toc(water1, dose=15)
+expect_s4_class(dosed_water, "water")
+})
+
+
+
+test_that("pac_toc works", {
+  water1 <- suppressWarnings(define_water(doc=2.5, uv254=.05))
+  water2 <- pac_toc(water1, dose=15, time = 50)
+  expect_equal(round(water2@doc, 2), .86)
+  expect_equal(round(water2@uv254,3), .86) 
+  
+  water3 <- pac_toc(water1, dose=15, time=50, type="wood")
+  expect_equal(round(water3@doc,2),.86)
+  
+  water4 <- pac_toc(water1, dose=15, time=50, type="lignite")
+  expect_equal(round(water4@doc,2), .86)
+})
+
+#check expect equal values above, these are just placeholders for now
+
+
