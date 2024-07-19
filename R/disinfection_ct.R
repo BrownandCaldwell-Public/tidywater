18fe3c1a7dcbba9f07e4915a5be00038c8327e26
Sierra Johnson <sjohnson2@brwncald.com>
2024-07-19 01:00:13 -0600

    formatting and stray paren

--- R/disinfection_ct.R
+++ R/disinfection_ct.R
@@ -1,26 +1,24 @@
 # CT Calculations
 
 chemdose_ct <- function(water, time, residual, baffle, volume, flow) {
-
   ph <- water@ph
   temp <- water@temp
 
-  if(missing(time) & !missing(volume) & !missing(flow)) {
+  if (missing(time) & !missing(volume) & !missing(flow)) {
     time <- volume / flow
   }
 
-  if(temp < 12.5) {
-    ct_required = (.353*.5)*(12.006 +exp(2.46-.073*temp+.125*residual+.389*ph))
-    log_removal = (residual*time*baffle)/(12.006 +exp(2.46-.073*temp+.125*residual+.389*ph))*1/.353
+  if (temp < 12.5) {
+    ct_required <- (.353 * .5) * (12.006 + exp(2.46 - .073 * temp + .125 * residual + .389 * ph))
+    log_removal <- (residual * time * baffle) / (12.006 + exp(2.46 - .073 * temp + .125 * residual + .389 * ph)) * 1 / .353
   } else {
-    ct_required <- (.361*0.5)*(-2.216 +exp(2.69-.065*temp+.111*residual+.361*ph))
-    log_removal = (residual*time*baffle)/(-2.216 +exp(2.69-.065*temp+.111*residual+.361*ph)))/.361)
+    ct_required <- (.361 * 0.5) * (-2.216 + exp(2.69 - .065 * temp + .111 * residual + .361 * ph))
+    log_removal <- (residual * time * baffle) / (-2.216 + exp(2.69 - .065 * temp + .111 * residual + .361 * ph)) / .361
   }
 
-  ct_actual = residual*time*baffle
+  ct_actual <- residual * time * baffle
 
   # What to actually return?
-
 }
 
 ozonate_ct <- function(water, time, residual) {
