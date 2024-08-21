29d875bad3c86d24032ada58774df54ce65fe91b
Sierra Johnson <sjohnson2@brwncald.com>
2024-08-21 10:19:33 -0600

    add bdoc

--- R/Biofilteration_model.R
+++ R/Biofilteration_model.R
@@ -12,6 +12,7 @@
 #' @examples
 #' example1 <- define_water(ph = 7, temp = 15, alk = 100, toc = 4, doc = 3.8, uv254 = .1) %>%
 #'   biofilter_toc(ebct = 20)
+#' example1@toc
 #'
 #' example2 <- define_water(ph = 7, temp = 15, alk = 100, toc = 4, doc = 3.8, uv254 = .1) %>%
 #'   biofilter_toc(ebct = 20, o3_dose = 3)
@@ -31,33 +32,36 @@ biofilter_toc <- function(water, ebct, o3_dose = 0) {
 
   # Determine the median k' value based on temperature and oxidation condition
   if (o3_dose == 0) {
+    bdoc <- 0.2 * water@doc
     if (temperature < 10) {
-      k_prime <- 0.05
+      k_prime <- 0.03
     } else if (temperature <= 20) {
       k_prime <- 0.09
     } else {
       k_prime <- 0.11
     }
   } else if (o3_dose > 0) {
+    bdoc <- 0.3 * water@doc
     if (temperature < 10) {
-      k_prime <- 0.04
+      k_prime <- 0.03
     } else if (temperature <= 20) {
-      k_prime <- 0.09
+      k_prime <- 0.06
     } else {
       k_prime <- 0.15
     }
   }
   # Calculate the ratio c/c_inf using the pseudo-first-order model
   c_cinf_ratio <- exp(-k_prime * ebct)
+  doc_removed <- bdoc - (bdoc * c_cinf_ratio)
 
   # Adjust water TOC based on the calculated TOC removal and c/c_inf ratio
   if (!is.na(water@toc)) {
-    water@toc <- water@toc * c_cinf_ratio
+    water@toc <- water@toc - doc_removed
   } else if (is.na(water@toc)) {
     warning("Input water TOC not specified. Output water TOC will be NA.")
   }
 
-  water@doc <- water@doc * c_cinf_ratio
+  water@doc <- water@doc - doc_removed
   water@applied_treatment <- paste(water@applied_treatment, "_biofilter", sep = "")
   return(water)
 }
