70b88c9efb988eefe511f2bbd2c5fc861101203a
phoebechen19 <pchen@brwncald.com>
2024-08-28 12:14:04 -0700

    Chlorine/Chloramine Decay Model
    Second draft of the chlorine/chloramine decay model. Notable Changes:
    1. Consolidated the uniroot function for chlorine/chloramine decay
    2. Changed naming
    3. Added option for chlorine correction (True/False)

--- R/chlorine_chloramine_decay.R
+++ R/chlorine_chloramine_decay.R
@@ -3,14 +3,16 @@
 
 #' @title Calculate total OCL
 #'
-#' @description \code{chlordose} calculates the decay of chlorine based on the U.S. EPA's
+#' @description \code{chemdose_cl2} calculates the decay of chlorine based on the U.S. EPA's
 #' Water Treatment Plant Model (U.S. EPA, 2001). Required arguments include an object of class "water"
 #' created by \code{\link{define_water}} chlorine/chloramine dose, type, reaction time, and treatment applied (if any).
 #' The function also requires additional water quality parameters defined in \code{define_water}
 #' including TOC and UVA.
 #'
 #' @details The function will calculate haloacetic acids (HAA) as HAA5, and total trihalomethanes (TTHM).
-#' The function returns a new object of class "water" with predicted chlorine/chloramine residual concentrations.
+#' The function returns a new object of class "water" with predicted chlorine/chloramine residual concentrations, which
+#' is total/combined chlorine concentration. For free concentration, the chemdose_ph functions should be implemented after
+#' this function. 
 #' Use \code{summarise_wq} to quickly tabulate the results.
 #'
 #' @source Ct, raw: U.S. EPA (2001) equation 5-113
@@ -20,7 +22,7 @@
 #' @source See references list at: \url{https://github.com/BrownandCaldwell/tidywater/wiki/References}
 #'
 #' @param water Source water object of class "water" created by \code{\link{define_water}}
-#' @param cl2 Applied chlorine or chloramine dose (mg/L as Cl2). Model results are valid for doses between 0.995 and 41.7 mg/L for raw water, 
+#' @param cl2_dose Applied chlorine or chloramine dose (mg/L as cl2_dose). Model results are valid for doses between 0.995 and 41.7 mg/L for raw water, 
 #' and for doses between 1.11 and 24.7 mg/L for coagulated water.
 #' @param time Reaction time (hours). Chlorine decay model results are valid for reaction times between 0.25 and 120 hours.Chloramine decay model
 #' does not have a time restriction. 
@@ -29,27 +31,38 @@
 #' @param cl_type Type of chlorination applied, either "chlorine" (default) or "chloramine".
 #' @examples
 #' example_cl2 <- suppressWarnings(define_water(8, 20, 66, toc = 4, uv254 = .2, br = 50)) %>%
-#'   chlordose(cl2 = 2, time = 8)
+#'   chemdose_cl2(cl2_dose = 2, time = 8)
 #' example_cl2 <- suppressWarnings(define_water(7.5, 20, 66, toc = 4, uv254 = .2, br = 50)) %>%
-#'   chlordose(cl2 = 3, time = 168, treatment = "coag", location = "ds")
+#'   chemdose_cl2(cl2_dose = 3, time = 168, treatment = "coag", location = "ds")
 #'
 #' @export
 #'
-chlordose <- function(water, cl2, time, treatment, cl_type) {
+chemdose_cl2 <- function(water, cl2_dose, time, treatment = "raw", cl_type = "chlorine", chlorine_correction = FALSE) {
   toc = water@toc
   uv254 = water@uv254
   
+  
+  # define table for decay coefficients
+  clcoeffs <- tibble(
+    treatment = c('raw','coag','either'),
+    a = c(-0.8147, -0.8404,-0.99),
+    b = c(-2.2808, -0.404,-0.015),
+    c = c(-1.2971, -0.9108, NA)
+  )
+  
+  # put in the same table for coefficients
+  # eventually put in the compiled script 
+  
 
   # Handle missing arguments with warnings (not all parameters are needed for all models).
-   if (missing(cl2) | missing(time)) {
-    stop("Missing value for cl2 or time. Please check the function inputs required to calculate chlorine/chloramine decay")
+   if (missing(cl2_dose) | missing(time)) {
+    stop("Missing value for cl2_dose or time. Please check the function inputs required to calculate chlorine/chloramine decay")
   }
 
   if ((cl_type != "chlorine" & cl_type != "chloramine") | missing(cl_type)) {
     stop("cl_type should be 'chlorine' or 'chloramine', or missing value for cl_type. Please check the spelling
          or add missing function inputs to calculate chlorine/chloramine decay.")
   }
-  
   
   # chlorine decay model 
   if (cl_type == "chlorine") {
@@ -69,7 +82,7 @@ chlordose <- function(water, cl2, time, treatment, cl_type) {
     }
 
     if (treatment == "coag" & (toc < 1.0 | toc > 11.1)) {
-    warning("For chlorine decay estimate, TOC is outside the model bounds of 1.0 <= TOC <= 11.1 mg/L for coagulated water")
+      warning("For chlorine decay estimate, TOC is outside the model bounds of 1.0 <= TOC <= 11.1 mg/L for coagulated water")
     }
 
     # uv254 warnings
@@ -81,121 +94,102 @@ chlordose <- function(water, cl2, time, treatment, cl_type) {
       warning("For chlorine decay estimate, uv254 is outside the model bounds of 0.012 <= UV254 <= 0.250 cm-1 for coagulated water.")
     }
 
-    # cl2 warnings
-    if (treatment == "raw" & (cl2 < 0.995 | cl2 > 41.7)) {
-      warning("For chlorine decay estimate, chlorine is outside the model bounds of 0.995 <= cl2 <= 41.7 mg/L for raw water")
+    # cl2_dose warnings
+    if (treatment == "raw" & (cl2_dose < 0.995 | cl2_dose > 41.7)) {
+      warning("For chlorine decay estimate, chlorine is outside the model bounds of 0.995 <= cl2_dose <= 41.7 mg/L for raw water")
     }
     
-    if (treatment == "coag" & (cl2 < 1.11 | cl2 > 24.7)) {
-      warning("For chlorine decay estimate, chlorine is outside the model bounds of 1.11 <= cl2 <= 24.7 mg/L for coagulated water")
+    if (treatment == "coag" & (cl2_dose < 1.11 | cl2_dose > 24.7)) {
+      warning("For chlorine decay estimate, chlorine is outside the model bounds of 1.11 <= cl2_dose <= 24.7 mg/L for coagulated water")
     }
   
     # time warning
     if (time < 0.25 | time > 120) {
       warning("For chlorine decay estimate, reaction time is outside the model bounds of 0.25 (15minutes) <= time <= 120 hours.")
     }
-
-    # define functions for chlorine decay
-    clcoeffs <- tibble(
-      treatment = c('raw','coag'),
-      a = c(-0.8147, -0.8404),
-      b = c(-2.2808, -0.404),
-      c = c(-1.2971, -0.9108)
-    )
     
-    cl_decay <- function(modeled_cl, a, b, cl2, uv254, c, toc, time) {
-      a * cl2 * log(cl2/modeled_cl) - b * (cl2/uv254)^c *toc*time + cl2 - modeled_cl
-    }
-    
-    solve_modeled_cl <- function(a, b, cl2, uv254, c, toc, time) {
-      uniroot(
-       cl_decay,
-       c(0.1,100),
-       a = a,
-       b = b,
-       cl2 = cl2,
-       uv254 = uv254,
-       c = c,
-       toc = toc,
-       time = time
-      )$root
-    }
-
-    # estimate chlorine decay based on level of treatment - results in ug/L
+    # get coefficients from defined clcoeffs table
     if (treatment == "raw") {
-      predicted_cl <- clcoeffs %>%
-        filter(treatment == "raw") %>%
-        rowwise() %>%
-        mutate(modeled_cl = solve_modeled_cl(a, b, cl2, uv254, c, toc, time))
-        ungroup()
+      coeffs <- clcoeffs %>%
+        filter(treatment == "raw")
     } else if (treatment == "coag") {
-      predicted_cl <- clcoeffs %>%
-        filter(treatment == "coag") %>%
-        rowwise() %>%
-        mutate(modeled_cl = solve_modeled_cl(a, b, cl2, uv254, c, toc, time))
-        ungroup()
+      coeffs <- clcoeffs %>%
+        filter(treatment == "coag")
     }
-
-
-    # apply chlorine residual correction, no significant difference in the parameters for different treatment
-    corrected_modeled_cl <- cl2 + (predicted_cl - cl2) / 0.85
-    water@ocl <- corrected_model_cl
-
-  } 
-  
-  
-    #chloramine decay model
-  else if (cl_type == "chloramine") { 
+    
+    # define function for chlorine decay
+    solve_decay <- function(Ct, a, b, cl2_dose, uv254, c, toc, time) {
+      a * cl2_dose * log(cl2_dose/Ct) - b * (cl2_dose/uv254)^c *toc*time + cl2_dose - Ct
+    }
+    
+  #chloramine decay model
+  } else if (cl_type == "chloramine") { 
     
     # chloramine decay model stop
     if (is.na(uv254)) {
       stop("For chloramine decay estimate, missing value for uv254 for chloramine decay estimate. 
            Please add missing parameters to define_water.")
     }
- 
-      cacoeffs <- tibble(
-      a = -0.99,
-      b = -0.015
-    )
     
-    # define functions for chloramine decay
-    ca_decay <- function(modeled_ca, a, b, cl2, uv254, time) {
-      a * cl2 * log(cl2/modeled_ca) - b * uv254 * time + cl2 - modeled_ca
+    # define function for chloramine decay
+    solve_decay <- function(CAt, a, b, cl2_dose, uv254, time) {
+      a * cl2_dose * log(cl2_dose/CAt) - b * uv254 * time + cl2_dose - CAt
     }
     
-    solve_modeled_ca <- function(a, b, cl2, uv254, time) {
-      uniroot(
-        ca_decay,
-        c(0.1,100),
-        a = a,
-        b = b,
-        cl2 = cl2,
-        uv254 = uv254,
-        time = time
-      )$root
-    }
-  
-    # estimate chloramine decay
-    predicted_cl <- cacoeffs %>%
-    rowwise() %>%
-    mutate(modeled_ca = solve_modeled_ca(a, b, cl2, uv254, time))%>%
-    ungroup()
-  
-    water@ocl <- predicted_cl
+    coeffs <- clcoeffs %>%
+      filter(treatment == "either")
+     
   }
+  
+  # results in ug/L
+  root_Ct <- stats::uniroot(solve_decay, interval = c(1e-14, cl2_dose),
+     a = coeffs$a,
+     b = coeffs$b,
+     c = coeffs$c,
+     cl2_dose = cl2_dose,
+     uv254 = uv254,
+     toc = toc,
+     time = time)$root
+  
+  
+  if ((cl_type == "chlorine") & (chlorine_correction == TRUE)) {
+    warning("Be mindful about the corrected chlorine residual concentration, because the correction 
+    applies to the initial Chlorine residual concentration, while the model input cl2_dose is the initial dose")
+    Ct <-  cl2_dose + (root_Ct - cl2_dose) / 0.85
     
+  } else {
+    Ct <- root_Ct
+  }
+
+  water@tot_ocl <- Ct
+  
+  # only impacts the tot_ocl, apply chemdose_ph if want ocl
+
   
   return(water)
   
-}
+}  
 
-#-------------------------------------
-# setwd("C:/Users/PChen/Rscripts/tidywater/R")
-# library(tidywater,tidyverse)
 
-# devtools::load_all()
-# example_cl2 <- suppressWarnings(define_water(8, 20, 66, toc = 4, uv254 = .2)) %>%
-# chlordose(cl2 = 2, time = 8, treatment = "raw", cl_type = "chlorine")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ tests/testthat/test-chlorine_chloramine_decay.R
@@ -0,0 +1,85 @@
+# Chemdose chlorine/chloramine ----
+
+test_that("chemdose_cl2 returns no modeled chlorine/chloramine residual when chlorine dose is 0.", {
+  water1 <- suppressWarnings(define_water(7.5, 20, 66, toc = 4, uv254 = .2))
+  Ct <- suppressWarnings(chemdose_cl2(water1, cl2_dose = 0, time = 8, treatment = 'raw', cl_type = 'chlorine'))
+
+  expect_equal(water1@ocl, 0)
+})
+
+test_that("chemdose_cl2 does not run when water_type isn't supplied correctly.", {
+  water1 <- suppressWarnings(define_water(ph = 7, toc = 3.5, uv254 = 0.1))
+
+  expect_error(chemdose_cl2(water1, water_type = "raw"))
+  expect_error(chemdose_cl2(water1, water_type = treated))
+})
+
+test_that("chemdose_cl2 warns when inputs are out of model range", {
+  water1 <- suppressWarnings(define_water(ph = 7.5, temp = 20, toc = 3.5, uv254 = 0.1))
+  water2 <- suppressWarnings(define_water(ph = 7.5, temp = 20, toc = .1, uv254 = 0.01))
+  water3 <- suppressWarnings(define_water(ph = 8, temp = 20, toc = 3, uv254 = 0.1))
+  water4 <- suppressWarnings(define_water(ph = 7.5, temp = 20, toc = 3, uv254 = 0.1))
+
+  expect_warning(chemdose_cl2(water1, cl2 = 1, time = 8)) # chlorine out of bounds
+  expect_warning(chemdose_cl2(water1, cl2 = 4, time = 1)) # time out of bounds
+  expect_warning(chemdose_cl2(water2, cl2 = 2, time = 8, treatment = "gac")) # toc out of bounds
+  expect_warning(chemdose_cl2(water3, cl2 = 4, time = 8, treatment = "coag")) # ph not set to 7.5
+  expect_warning(chemdose_cl2(water4, cl2 = 4, time = 8)) # br out of bounds
+})
+
+test_that("chemdose_cl2 stops working when inputs are missing", {
+  water1 <- suppressWarnings(define_water(toc = 3.5, uv254 = 0.1))
+  water2 <- suppressWarnings(define_water(ph = 7.5, uv254 = 0.1))
+  water3 <- suppressWarnings(define_water(ph = 8, toc = 3, br = 50))
+  water4 <- suppressWarnings(define_water(ph = 8, toc = 3, uv = 0.2))
+  water5 <- suppressWarnings(define_water(ph = 8, temp = 25, toc = 3, uv = 0.2))
+
+  expect_error(chemdose_cl2(water1, cl2 = 4, time = 8)) # missing ph
+  expect_error(chemdose_cl2(water2, cl2 = 4, time = 8)) # missing toc
+  expect_error(chemdose_cl2(water3, cl2 = 4, time = 1, treatment = "coag")) # missing uv
+  expect_no_error(suppressWarnings(chemdose_cl2(water3, cl2 = 4, time = 1, treatment = "raw"))) # raw doesn't require uv
+  expect_error(chemdose_cl2(water4, cl2 = 4, time = 8)) # missing br
+  expect_error(chemdose_cl2(water5, time = 8)) # missing cl2
+  expect_error(chemdose_cl2(water5, cl2 = 4)) # missing time
+
+})
+
+
+test_that("chemdose_cl2 works.", {
+  water1 <- suppressWarnings(define_water(ph = 7.5, temp = 20, toc = 3.5, uv254 = 0.1, br = 50))
+  water2 <- chemdose_cl2(water1, cl2 = 3, time = 8)
+  water3 <- chemdose_cl2(water1, cl2 = 3, time = 8, treatment = "coag")
+  water4 <- chemdose_cl2(water1, cl2 = 3, time = 72, treatment = "coag", location = "ds")
+  water5 <- suppressWarnings(define_water(ph = 7.5, temp = 20, toc = 1, uv254 = 0.04, br = 50))
+  water6 <- chemdose_cl2(water5, cl2 = 3, time = 8, treatment = "gac")
+
+  expect_equal(round(water2@ocl), 68)
+  expect_equal(round(water3@ocl), 59)
+  expect_equal(round(water3@haa5), 48)
+  expect_equal(round(water4@haa5), 69)
+  expect_equal(round(water6@haa5), 12)
+})
+
+
+
+
+
+
+# refer to uniroot template (acid_base function)
+# only modify coeffs in if loops
+# cl2 -- cl2_dose
+# modeled_cl -- cl2_t / Ct
+# check with sierra for function naming convention 
+
+#-------------------------------------
+# setwd("C:/Users/PChen/Rscripts/tidywater/R")
+# library(tidywater,tidyverse)
+
+# devtools::load_all()
+# example_cl2 <- suppressWarnings(define_water(8, 20, 66, toc = 4, uv254 = .2)) %>%
+# chemdose_cl2(cl2 = 2, time = 8, treatment = "raw", cl_type = "chlorine")
+
+# water1 <- suppressWarnings(define_water(7.5, 20, 66, toc = 4, uv254 = .2))
+# test <- suppressWarnings(chemdose_cl2(water1, cl2_dose = 1, time = 8, treatment = 'raw', cl_type = 'chlorine'))
+
+#-------------------------------------
