ec950375eb54d24ca8d8ff18c7da6ad2ee11885d
mayushiva1707 <MNamasivayam@BrwnCald.com>
2024-09-05 14:24:27 -0700

    Update Biofilteration_model.R

--- R/Biofilteration_model.R
+++ R/Biofilteration_model.R
@@ -1,156 +1,89 @@
 # TOC models
 
-
-#' @title Determine TOC removal from coagulation
+#' @title Determine TOC removal using BDOC model
 #'
-#' @description This function applies the Edwards (1997) model to a water created by \code{\link{define_water}} to determine coagulated
-#' DOC. Coagulated UVA is from U.S. EPA (2001) equation 5-80. Note that the models rely on pH of coagulation. If
-#' only raw water pH is known, utilize \code{\link{chemdose_ph}} first.
+#' @description This function calculates TOC removal based on BDOC removal using different rate constants for various conditions.
 #'
-#' @param water Source water object of class "water" created by \code{\link{define_water}}. Water must include ph, doc, and uv254
-#' @param alum Amount of hydrated aluminum sulfate added in mg/L: Al2(SO4)3*14H2O + 6HCO3 -> 2Al(OH)3(am) +3SO4 + 14H2O + 6CO2
-#' @param fecl3 Amount of ferric chloride added in mg/L: FeCl3 + 3HCO3 -> Fe(OH)3(am) + 3Cl + 3CO2
-#' @param fe2so43 Amount of ferric sulfate added in mg/L: Fe2(SO4)3 + 6HCO3 -> 2Fe(OH)3(am) +3SO4 + 6CO2
-#' @param coeff String specifying the Edwards coefficients to be used from "Alum", "Ferric", "General Alum", "General Ferric", or "Low DOC" or
-#' named vector of coefficients, which must include: k1, k2, x1, x2, x3, b
-#'
-#' @seealso \code{\link{chemdose_ph}}
-#'
-#' @source Edwards (1997)
-#' @source U.S. EPA (2001)
-#' @source See reference list at: \url{https://github.com/BrownandCaldwell/tidywater/wiki/References}
+#' @param water Source water object of class "water" created by \code{\link{define_water}}.
+#' @param EBCT Empty Bed Contact Time in minutes
+#' @param temp Temperature in °C
+#' @param ozonated Logical; TRUE if the water is ozonated, FALSE otherwise
 #'
 #' @examples
-#' water <- define_water(ph = 7, temp = 25, alk = 100, toc = 3.7, doc = 3.5, uv254 = .1)
-#' dosed_water <- chemdose_ph(water, alum = 30) %>%
-#'   chemdose_toc(alum = 30, coeff = "Alum")
-#'
-#' dosed_water <- chemdose_ph(water, fe2so43 = 30) %>%
-#'   chemdose_toc(fe2so43 = 30, coeff = "Ferric")
-#'
-#' dosed_water <- chemdose_ph(water, alum = 10, h2so4 = 10) %>%
-#'   chemdose_toc(alum = 10, coeff = c("x1" = 280, "x2" = -73.9, "x3" = 4.96, "k1" = -0.028, "k2" = 0.23, "b" = 0.068))
+#' water <- define_water(ph = 7, temp = 25, alk = 100, toc = 5.0, doc = 4.0, uv254 = .1)
+#' dosed_water <- chemdose_bdoc(water, EBCT = 10, temp = 15, ozonated = TRUE)
 #'
 #' @export
-
-#'Define the Water Object Function
-# define_water <- function(ph, temp, alk, toc, doc, uv254) {
-#   structure(list(ph = ph, temp = temp, alk = alk, toc = toc, doc = doc, uv254 = uv254, treatment = ""), class = "water")
-# }
-
-# Biofilter TOC Function
-biofilter_toc <- function(water, ebct, temperature, media_type = "GAC", oxidation_condition = "non-ozonated", o3_toc_ratio = NA) {
-
-  # Check if water object is missing or not of class 'water'
+#'
+chemdose_bdoc <- function(water, EBCT, temp, ozonated) {
+  
   if (missing(water)) {
     stop("No source water defined. Create a water using the 'define_water' function.")
   }
-  if (!inherits(water, "water")) {
+  if (!methods::is(water, "water")) {
     stop("Input water must be of class 'water'. Create a water using 'define_water'.")
   }
-
-  # Define the model parameters
-  intercept <- 0.132
-  media_coefficients <- list(GAC = 0.087, sand = 0.074, anthracite = 0.065)
-  o3_toc_coefficient <- 0.053
-  ebct_coefficient <- 0.039
-  ebct_o3_toc_coefficient <- 0.014
-
-  # Get the media coefficient
-  if (!media_type %in% names(media_coefficients)) {
-    stop("Invalid media type. Choose from 'GAC', 'sand', or 'anthracite'.")
+  
+  if (is.na(water@toc)) {
+    stop("Water is missing a TOC parameter. Make sure TOC is specified.")
   }
-  media_coefficient <- media_coefficients[[media_type]]
-
-  # Determine the median k' value based on temperature and oxidation condition
-  if (oxidation_condition == "non-ozonated") {
-    if (temperature < 10) {
-      k_prime <- 0.05
-    } else if (temperature <= 20) {
-      k_prime <- 0.09
+  
+  # Define BDOC fractions
+  BDOC_fraction_nonozonated <- 0.2
+  BDOC_fraction_ozonated <- 0.3
+  
+  # Determine BDOC fraction and rate constant k' based on temperature and ozonation
+  if (ozonated) {
+    if (temp <= 10) {
+      k <- 0.03
+      BDOC_fraction <- BDOC_fraction_ozonated
+    } else if (temp <= 20) {
+      k <- 0.06
+      BDOC_fraction <- BDOC_fraction_ozonated
     } else {
-      k_prime <- 0.11
-    }
-  } else if (oxidation_condition == "ozonated") {
-    if (temperature < 10) {
-      k_prime <- 0.04
-    } else if (temperature <= 20) {
-      k_prime <- 0.09
-    } else {
-      k_prime <- 0.15
+      k <- 0.15
+      BDOC_fraction <- BDOC_fraction_ozonated
     }
   } else {
-    stop("Invalid oxidation condition. Choose from 'non-ozonated' or 'ozonated'.")
+    if (temp <= 10) {
+      k <- 0.03
+      BDOC_fraction <- BDOC_fraction_nonozonated
+    } else if (temp <= 20) {
+      k <- 0.09
+      BDOC_fraction <- BDOC_fraction_nonozonated
+    } else {
+      k <- 0.11
+      BDOC_fraction <- BDOC_fraction_nonozonated
+    }
   }
-
-  # Calculate the ratio c/c_inf using the pseudo-first-order model
-  c_cinf_ratio <- exp(-k_prime * ebct)
-
-  # Calculate TOC removal using the model
-  if (oxidation_condition == "non-ozonated") {
-    tocremoval <- intercept + media_coefficient + (ebct_coefficient * ebct)
-  } else if (oxidation_condition == "ozonated") {
-    tocremoval <- intercept + media_coefficient + (o3_toc_coefficient * o3_toc_ratio) +
-      (ebct_coefficient * ebct) + (ebct_o3_toc_coefficient * ebct * o3_toc_ratio)
-  } else {
-    stop("Invalid oxidation condition. Choose from 'non-ozonated' or 'ozonated'.")
-  }
-
-  # Adjust water TOC based on the calculated TOC removal and c/c_inf ratio
-  if (!is.na(water@toc) & water@toc >= water@doc) {
-    water@toc <- water@toc - (water@toc * tocremoval / 100) * c_cinf_ratio
-  } else if (!is.na(water@toc) & water@toc < water@doc) {
-    warning("TOC of input water less than DOC. TOC will be set equal to DOC.")
-    water@toc <- water@doc - (water@doc * tocremoval / 100) * c_cinf_ratio
-  } else if (is.na(water@toc)) {
-    warning("Input water TOC not specified. Output water TOC will be NA.")
-    water@toc <- NA_real_
-  }
-
-  water@doc <- water@toc  # Assuming DOC is a subset of TOC in this simplified model
-  water@treatment <- paste(water@treatment, "_tocremoved", sep = "")
-
-  # Convert to data frame
-  water_df <- data.frame(
-    ph = water@ph,
-    temp = water@temp,
-    alk = water@alk,
-    toc = water@toc,
-    doc = water@doc,
-    uv254 = water@uv254,
-    treatment = water@treatment
-  )
-
-  return(water_df)
+  
+  # Calculate BDOC influent concentration
+  BDOC_inf <- BDOC_fraction * water@toc
+  
+  # Calculate BDOC effluent concentration using the exponential decay model
+  BDOC_eff <- BDOC_inf * exp(-k * EBCT)
+  
+  # Calculate TOC removal percentage
+  TOC_removal <- (BDOC_inf - BDOC_eff) / water@toc
+  
+  # Update water object with new TOC and DOC values
+  water@toc <- water@toc * (1 - TOC_removal)
+  water@doc <- water@toc * BDOC_fraction
+  
+  # Append treatment description
+  water@treatment <- paste(water@treatment, "_bdocremoved", sep = "")
+  
+  return(water)
 }
 
-# Example usage
-# Define your water object with your specific values
-water <- define_water(ph = 7, temp = 25, alk = 100, toc = 3.7, doc = 3.5, uv254 = .1)
-#
-# # Run the biofilter_toc function with your values and print the results
-#
-# # For non-ozonated condition using GAC
-filtered_water_non_ozonated_gac <- biofilter_toc(water, ebct = 20, temperature = 15, media_type = "GAC", oxidation_condition = "non-ozonated")
-print(filtered_water_non_ozonated_gac)
-#
-# # For non-ozonated condition using sand
-# filtered_water_non_ozonated_sand <- biofilter_toc(water, ebct = 20, temperature = 15, media_type = "sand", oxidation_condition = "non-ozonated")
-# print(filtered_water_non_ozonated_sand)
-#
-# # For non-ozonated condition using anthracite
-# filtered_water_non_ozonated_anthracite <- biofilter_toc(water, ebct = 20, temperature = 15, media_type = "anthracite", oxidation_condition = "non-ozonated")
-# print(filtered_water_non_ozonated_anthracite)
-#
-# # For ozonated condition using GAC
-# filtered_water_ozonated_gac <- biofilter_toc(water, ebct = 20, temperature = 15, media_type = "GAC", oxidation_condition = "ozonated", o3_toc_ratio = 0.8)
-# print(filtered_water_ozonated_gac)
-#
-# # For ozonated condition using sand
-# filtered_water_ozonated_sand <- biofilter_toc(water, ebct = 20, temperature = 15, media_type = "sand", oxidation_condition = "ozonated", o3_toc_ratio = 0.8)
-# print(filtered_water_ozonated_sand)
-#
-# # For ozonated condition using anthracite
-# filtered_water_ozonated_anthracite <- biofilter_toc(water, ebct = 20, temperature = 15, media_type = "anthracite", oxidation_condition = "ozonated", o3_toc_ratio = 0.8)
-# print(filtered_water_ozonated_anthracite)
-#
+# SUVA calculation function remains unchanged
+calc_suva <- function(doc, uv254) {
+  uv254 / doc * 100
+}
+
+water <- define_water(ph = 7, temp = 15, alk = 100, toc = 5.0, doc = 4.0, uv254 = 0.1)
+
+dosed_water <- chemdose_bdoc(water, EBCT = 10, temp = 15, ozonated = TRUE)
+
+print(dosed_water)
+
