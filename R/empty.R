307a55ee16c249c9afc5f9f89d4b6de85e8e775b
Sierra Johnson <sjohnson2@brwncald.com>
2024-09-23 10:47:38 -0600

    create mostly empty package

---
+++ R/empty.R
@@ -0,0 +1,14 @@
+# Use imported packages and test file.
+#' @title empty function
+#' @import dplyr
+#' @import furrr
+#' @import ggplot2
+#' @import ggrepel
+#' @import methods
+#' @import purrr
+#' @import tidyr
+#'
+#' @export
+empty <- function() {
+  print("hi")
+}
