6563ce533fc0aabc146e3679ed1168251ef6585b
Sierra Johnson <80361498+sierrajohnson@users.noreply.github.com>
2024-10-01 10:26:37 -0400

    Create empty package (#1)
    * create mostly empty package
    
    * Update LICENSE
    
    Co-authored-by: Sierra Johnson <80361498+sierrajohnson@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Chris Somerlot <csomerlot@brwncald.com>

---
+++ R/tidywater-package.R
@@ -0,0 +1,10 @@
+#' @keywords internal
+"_PACKAGE"
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+utils::globalVariables(".")
+
+## usethis namespace: start
+#' @importFrom rlang :=
+## usethis namespace: end
+NULL
