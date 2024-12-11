f3a09e69fde592d85fb42d83fa9957e0acf759c1
Libby McKenna <libbydmckenna@gmail.com>
2024-12-11 10:56:19 -0700

    add tot_po4, tot_nh3, and tot_ocl with corrections

--- R/convert_pluck_water.R
+++ R/convert_pluck_water.R
@@ -63,10 +63,12 @@ convert_watermg <- function(water) {
   if (!methods::is(water, "water")) {
     stop("Input water must be of class 'water'. Create a water using 'define_water'.")
   }
+  ks <- correct_k(water)
+  h <- 10^-ph
 
-  water@na <- convert_units(water@na, "na", "M", "mg/L")
   water@ca <- convert_units(water@ca, "ca", "M", "mg/L")
   water@mg <- convert_units(water@mg, "mg", "M", "mg/L")
+  water@na <- convert_units(water@na, "na", "M", "mg/L")
   water@k <- convert_units(water@k, "k", "M", "mg/L")
   water@cl <- convert_units(water@cl, "cl", "M", "mg/L")
   water@so4 <- convert_units(water@so4, "so4", "M", "mg/L")
@@ -75,13 +77,15 @@ convert_watermg <- function(water) {
   water@h2po4 <- convert_units(water@h2po4, "h2po4", "M", "mg/L")
   water@hpo4 <- convert_units(water@hpo4, "hpo4", "M", "mg/L")
   water@po4 <- convert_units(water@po4, "po4", "M", "mg/L")
+  water@tot_po4 <- water@h2po4/calculate_alpha1_phosphate(h, ks) +  water@hpo4/calculate_alpha2_phosphate(h, ks) + water@po4/calculate_alpha3_phosphate(h, ks)
   water@ocl <- convert_units(water@ocl, "ocl", "M", "mg/L")
-  water@tot_nh3 <- convert_units(water@nh4, "nh4", "M", "mg/L N")
+  water@tot_ocl <- water@ocl/ calculate_alpha1_hypochlorite(h, ks)
+  water@tot_nh3 <- convert_units(water@nh4, "nh4", "M", "mg/L N") / calculate_alpha1_ammonia(h, ks)
 
-  water@bro3 <- convert_units(water@bro3, "bro3", "M", "mg/L")
   water@f <- convert_units(water@f, "f", "M", "mg/L")
   water@fe <- convert_units(water@fe, "fe", "M", "mg/L")
   water@al <- convert_units(water@al, "al", "M", "mg/L")
+  water@bro3 <- convert_units(water@bro3, "bro3", "M", "mg/L")
 
   # These get converted to ug/L instead.
   water@br <- convert_units(water@br, "br", "M", "ug/L")
