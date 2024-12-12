6898a7d20faba49b4ffce8214a3d834705e7e8fa
Libby McKenna <libbydmckenna@gmail.com>
2024-12-12 12:27:30 -0700

    def water chain tot_nh3

--- R/define_water.R
+++ R/define_water.R
@@ -315,7 +315,7 @@ define_water_once <- function(df) {
 
 define_water_chain <- function(df, output_water = "defined_water") {
   define_water_args <- c(
-    "ph", "temp", "alk", "tot_hard", "ca", "mg", "na", "k", "cl", "so4", "tot_ocl", "tot_po4", "tot_nh4",
+    "ph", "temp", "alk", "tot_hard", "ca", "mg", "na", "k", "cl", "so4", "tot_ocl", "tot_po4", "tot_nh3",
     "tds", "cond",
     "toc", "doc", "uv254", "br", "f", "fe", "al", "mn"
   )
