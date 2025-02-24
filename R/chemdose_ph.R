035929f856a40dd67e678f5d8520cca3e6c15a92
Sierra Johnson <80361498+sierrajohnson@users.noreply.github.com>
2025-02-24 11:36:33 -0700

    Cranfixes (#309)

--- R/chemdose_ph.R
+++ R/chemdose_ph.R
@@ -283,6 +283,7 @@ chemdose_ph <- function(water, hcl = 0, h2so4 = 0, h3po4 = 0, co2 = 0,
 #'   ) %>%
 #'   chemdose_ph_once(input_water = "balanced_water", mgoh2 = 55, co2 = 4)
 #'
+#' \donttest{
 #' # Initialize parallel processing
 #' plan(multisession, workers = 2) # Remove the workers argument to use all available compute
 #' example_df <- water_df %>%
@@ -292,6 +293,7 @@ chemdose_ph <- function(water, hcl = 0, h2so4 = 0, h3po4 = 0, co2 = 0,
 #'
 #' # Optional: explicitly close multisession processing
 #' plan(sequential)
+#' }
 #'
 #' @import dplyr
 #' @importFrom tidyr unnest
@@ -384,6 +386,7 @@ chemdose_ph_once <- function(df, input_water = "defined_water",
 #'   ) %>%
 #'   chemdose_ph_chain(input_water = "balanced_water", mgoh2 = 55, co2 = 4)
 #'
+#' \donttest{
 #' # Initialize parallel processing
 #' plan(multisession, workers = 2) # Remove the workers argument to use all available compute
 #' example_df <- water_df %>%
@@ -393,6 +396,7 @@ chemdose_ph_once <- function(df, input_water = "defined_water",
 #'
 #' # Optional: explicitly close multisession processing
 #' plan(sequential)
+#' }
 #'
 #' @import dplyr
 #' @export
