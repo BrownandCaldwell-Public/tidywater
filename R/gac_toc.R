dddc17a748716dd5891b96ad57c3b23a100a08e9
Jiaming Yuan <jyuan@brwncald.com>
2025-06-30 13:59:58 -0600

    started adding in WTP model

--- R/gac_toc.R
+++ R/gac_toc.R
@@ -25,8 +25,10 @@
 #' @source Zachman and Summers (2010)
 #' 
 #' @param water Source water object of class "water" created by [define_water]
-#' @param media_size Size of GAC filter mesh. Model includes 12x40 and 8x30 mesh sizes.
 #' @param ebct Empty bed contact time (minutes). Model results are valid for 10 or 20 minutes.
+#' @param model Specifies which GAC TOC removal model to apply. Options are Zachman and WTP.
+#' @param media_size Size of GAC filter mesh. Model includes 12x40 and 8x30 mesh sizes.
+#' @param RT Blended run time of the GAC filter in days. Common run times range from 0.5 to 3 days.
 #' @param option Specifies the type of output: plot produces the breakthrough curve, fin_water outputs a water with updated toc/doc, and
 #' bvs will calculate bed volume required for target final doc. Argument can take multiple inputs.
 #' @param bed_vol Optional input to calculate breakthrough for a given bed volume
@@ -34,14 +36,15 @@
 #'
 #' @examples
 #' water <- define_water(ph = 8, toc = 2.5, uv254 = .05, doc = 1.5) %>%
-#'   gac_toc(media_size = "8x30", ebct = 20, option = "plot")
+#'   gac_toc(media_size = "8x30", ebct = 20, model = "Zachman", option = "plot")
 #'
 #' @export
 #'
 #' @returns `gac_toc` returns a water class object with updated DOC, TOC, and UV254 slots.
 #'
 
-gac_toc <- function(water, media_size = "12x40", ebct = 10, option = "fin_water", bed_vol, target_doc) {
+gac_toc <- function(water, ebct = 10, model, media_size = "12x40", RT = 2, 
+                    option = "fin_water", bed_vol, target_doc) {
   validate_water(water, c("ph", "toc"))
   
   # check that media_size, ebct, and option are inputted correctly
@@ -54,26 +57,47 @@ gac_toc <- function(water, media_size = "12x40", ebct = 10, option = "fin_water"
   }
   
   if (!all(option %in% c("fin_water", "plot", "bvs"))) {
-    stop("Please choose from the three options: fin_water, plot, and bvs.")
+    stop("Please choose from the three output options: fin_water, plot, and bvs.")
   }
   
-  x_norm <- seq(20, 70, 0.5) # x_norm represents the normalized effluent TOC concentration
-  
-  # Equations for A and BV according to Zachman and Summers 2010
-  if (media_size == "12x40" && ebct == 10) {
-    A = 196*x_norm^2-5589*x_norm+252922
-  } else if (media_size == "12x40" && ebct == 20) {
-    A = 164*x_norm^2-1938*x_norm+245064
-  } else if (media_size == "8x30" && ebct == 10) {
-    A = 178*x_norm^2-6208*x_norm+238321
+  if (model == "Zachman") {
+    x_norm <- seq(20, 70, 0.5) # x_norm represents the normalized effluent TOC concentration
+    ### Implementation with the Zachman and Summers model
+    # Equations for A and BV according to Zachman and Summers 2010
+    if (media_size == "12x40" && ebct == 10) {
+      A = 196*x_norm^2-5589*x_norm+252922
+    } else if (media_size == "12x40" && ebct == 20) {
+      A = 164*x_norm^2-1938*x_norm+245064
+    } else if (media_size == "8x30" && ebct == 10) {
+      A = 178*x_norm^2-6208*x_norm+238321
+    } else {
+      A = 202*x_norm^2-5995*x_norm+261914
+    }
+    
+    bv <- A * water@toc^-1 * water@ph^-1.5 
+  } else if (model == "WTP") {
+    ### Implementation from the WTP Model v. 2.0 Manual
+    #inputs: ph_inf, toc_inf, RT, ebct (10 or 20)
+    # ebct = 10: 1.51 < toc_inf < 11.5 ; 6.07 < ph_inf < 9.95
+    # ebct = 20: 1.51 < toc_inf < 11.5 ; 6.14 < ph_inf < 9.95 
+    ph_base <- 7.93 # mean pH for which the model was developed
+    ebct_adj <- ebct * (1 - 0.044 * (ph_base - water@ph))
+    
+    A0 <- water@toc * ((-1.148 * 10^-3 * ebct_adj) + 1.208 * 10^-1) - 2.710 * 10^-6 * ebct_adj + 1.097 * 10^-5
+    Af <- water@toc * ((3.244 * 10^-3 * ebct_adj) + 5.383 * 10^-1) + 1.033 * 10^-5 * ebct_adj + 1.759 * 10^-5
+    D <- water@toc * ((-1.079 * 10^-5 * ebct_adj) + 4.457 * 10^-4) + 1.861 * 10^-5 * ebct_adj - 2.809 * 10^-4
+    B = 100
+    bv <- 1440 * RT / ebct_adj # is this bed volume?
+    
+    toc_eff <- A0 + Af + Af * (log(((1+B*exp(-D*bv))/(1+B)))/(D*bv))
+    x_norm <- toc_eff / water@toc # ranges 10 to 72.5%
+    
   } else {
-    A = 202*x_norm^2-5995*x_norm+261914
+    stop("Please choose either Zachman or WTP as the model.")
   }
-  
-  bv <- A * water@toc^-1 * water@ph^-1.5
   
   output <- list()
-  if ("plot" %in% options) {
+  if ("plot" %in% option) {
     # plot the breakthrough curve
     breakthrough <- data.frame(bv = bv, x_norm = x_norm/100)
     plot <- ggplot(data = breakthrough, aes(bv, x_norm)) +
@@ -87,26 +111,25 @@ gac_toc <- function(water, media_size = "12x40", ebct = 10, option = "fin_water"
     print(plot)
   }
   
-  if ("fin_water" %in% options) {
+  if ("fin_water" %in% option) {
     if (missing(bed_vol)) {
       stop("Bed volume is a required argument to predict final water quality.")
     }
     
     bv_index <- which.min(abs(bv - bed_vol)) # retrieves index of bv that's closest to bv arg
     output_water <- water
-    output_water@toc = x_norm[bv_index] * water@toc # calculate toc from predicted breakthrough curve
+    output_water@toc <- x_norm[bv_index] * water@toc # calculate toc from predicted breakthrough curve
     
-    # Predict DOC concentration via UV absorbance
-
-    # UVA can be a good indicator to predict DOC concentration by PAC adsorption
-    # can be predicted through relationship of DOC and UVA removal --> dimensionless unit (C/C0)
-
-    # output_water@UVA <- .0376 * output_water@doc - .041
+    # from WTP manual model:
+    # if only coagulation before gac, 0.1 < x_norm*toc_inf < 14.7
+    output_water@uv254 <- 0.0195 * output_water@toc - 0.0077
+    # if coagulation, ozonation, and biotreatment before gac, 0.1 < x_norm*toc_inf < 6.1
+    output_water@uv254 <- 0.0014 * output_water@toc - 0.00141
     
     output <- append(output, output_water)
   }
   
-  if ("bvs" %in% options) {
+  if ("bvs" %in% option) {
     if (missing(target_doc)) {
       stop("Target DOC is a required argument to predict bed volumes.")
     }
@@ -117,6 +140,7 @@ gac_toc <- function(water, media_size = "12x40", ebct = 10, option = "fin_water"
   }
   
   return(output)
+  
 }
 
 # Currently gac_toc_chain function is copied from pac_toc_chain -- may not be implemented
