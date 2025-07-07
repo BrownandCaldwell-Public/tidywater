b1a35f2f6ee8464dc66ee0b4bff6d8b8ab4201e6
Jiaming Yuan <jyuan@brwncald.com>
2025-07-07 11:22:46 -0600

    minor fixes

--- R/gac_toc.R
+++ R/gac_toc.R
@@ -92,7 +92,7 @@ gac_toc <- function(water, ebct = 10, model, media_size = "12x40", bed_vol, pret
 #'# plan(multisession)
 #' example_df <- water_df %>%
 #'   define_water_chain("raw") %>%
-#'   gac_toc_chain(input_water = "raw")
+#'   gac_toc_chain(input_water = "raw", model = "WTP")
 #'
 #' # Optional: explicitly close multisession processing
 #'# plan(sequential)
