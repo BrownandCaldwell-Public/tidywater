0db5986daadc54a0c4d552b1550c5397d8f31962
Sierra Johnson <sjohnson2@brwncald.com>
2025-07-27 16:41:44 -0600

    small update

--- tests/testthat/test-rename_helpers.R
+++ tests/testthat/test-rename_helpers.R
@@ -1,12 +1,14 @@
 
 
 
-test_that("define_water_chain and balance_ions_chain works", {
+test_that("_chain works", {
   water0 <- water_df %>%
     define_water_chain()
 
   water1 <- balance_ions_chain(water0)
 
   water2 <- biofilter_toc_chain(water0, ebct = 10)
+
+  water3 <- chemdose_chloramine_chain(water0, time = 10, cl2 = 3, nh3 = .5)
 
 })
